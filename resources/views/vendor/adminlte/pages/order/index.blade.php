5d8686401ba445c8437129f8166e4c8ed24b4979
Eric Bernardo <eric.sousa@gmail.com>
2018-12-17 13:50:33 -0200

    Adicionado o scroll na listagem dos pedidos

--- resources/views/vendor/adminlte/pages/order/index.blade.php
+++ resources/views/vendor/adminlte/pages/order/index.blade.php
@@ -32,53 +32,58 @@
 
                 <div class="box-body no-padding">
 
-                    <table class="table table-striped table-hover">
-                        <tbody>
-                        <tr>
-                            <th style="width: 10px">#</th>
-                            <th>{{ trans('adminlte_lang::message.client') }}</th>
-                            <th>{{ trans('adminlte_lang::message.price') }}</th>
-                            <th>{{ trans('adminlte_lang::message.date') }}</th>
-                            <th>{{ trans('adminlte_lang::message.paid') }}?</th>
-                            <th>-</th>
-                            <th>-</th>
-                        </tr>
-                        @foreach($results as $result)
+                    <div class="table-responsive">
+
+                        <table class="table table-striped table-hover">
+                            <tbody>
                             <tr>
-                                <td style="width: 10px">{{$result['id']}}</td>
-                                <td>{{$result['client']['title']}}</td>
-                                <td>R$ {{number_format($result['total'],2,',','.')}}</td>
-                                <td>{{date('d/m/Y H:i', strtotime($result['created_at']))}}</td>
-                                <td>{{($result['paid']?'Sim': 'Não')}}</td>
-                                <td>
-                                    <a href="{{ route('order.edit', [ 'id' => $result['id'] ]) }}" class="btn btn-primary btn-sm">
-                                        <i class="fa fa-pencil" aria-hidden="true"></i>
-                                        {{ trans('adminlte_lang::message.edit') }}
-                                    </a>
-                                </td>
-                                <td>
-
-                                    <form method="POST"
-                                          action="{{ route('order.destroy', [ 'id' => $result['id'] ]) }}"
-                                          class="display-inline" role="form">
-
-                                        <input type="hidden" name="_method" value="DELETE">
-
-                                        {{ csrf_field() }}
-
-                                        <button type="submit" class="btn btn-danger btn-sm btn-delete"
-                                                title="{{ trans('adminlte_lang::message.delete') }}" data-destroy>
-                                            <i class="fa fa-trash" aria-hidden="true"></i>
-                                            {{ trans('adminlte_lang::message.delete') }}
-                                        </button>
-
-                                    </form>
-
-                                </td>
+                                <th style="width: 10px">#</th>
+                                <th>{{ trans('adminlte_lang::message.client') }}</th>
+                                <th>{{ trans('adminlte_lang::message.price') }}</th>
+                                <th>{{ trans('adminlte_lang::message.date') }}</th>
+                                <th>{{ trans('adminlte_lang::message.paid') }}?</th>
+                                <th>-</th>
+                                <th>-</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            @foreach($results as $result)
+                                <tr>
+                                    <td style="width: 10px">{{$result['id']}}</td>
+                                    <td>{{$result['client']['title']}}</td>
+                                    <td>R$ {{number_format($result['total'],2,',','.')}}</td>
+                                    <td>{{date('d/m/Y H:i', strtotime($result['created_at']))}}</td>
+                                    <td>{{($result['paid']?'Sim': 'Não')}}</td>
+                                    <td>
+                                        <a href="{{ route('order.edit', [ 'id' => $result['id'] ]) }}" class="btn btn-primary btn-sm">
+                                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                                            {{ trans('adminlte_lang::message.edit') }}
+                                        </a>
+                                    </td>
+                                    <td>
+
+                                        <form method="POST"
+                                              action="{{ route('order.destroy', [ 'id' => $result['id'] ]) }}"
+                                              class="display-inline" role="form">
+
+                                            <input type="hidden" name="_method" value="DELETE">
+
+                                            {{ csrf_field() }}
+
+                                            <button type="submit" class="btn btn-danger btn-sm btn-delete"
+                                                    title="{{ trans('adminlte_lang::message.delete') }}" data-destroy>
+                                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                                {{ trans('adminlte_lang::message.delete') }}
+                                            </button>
+
+                                        </form>
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+
+                    </div>
+
                 </div>
                 <!-- /.box-body -->
 
